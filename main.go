104a8e7f87818e303483ea231d74ed3e00f25068
fardinabir <fardin.abir@pathao.com>
2023-12-13 17:02:37 +0600

    Added base scapper

--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"github.com/spf13/viper"
-	"log"
-	"match_statistics_scrapper/job"
+	"match_statistics_scrapper/scrapper"
 )
 
 func main() {
-	viper.SetConfigFile("config.yaml")
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("Error reading config file: %s", err)
-	}
-
-	job.StartTicker()
+	//viper.SetConfigFile("config.yaml")
+	//if err := viper.ReadInConfig(); err != nil {
+	//	log.Fatalf("Error reading config file: %s", err)
+	//}
+	//
+	//job.StartTicker()
+	scrapper.Scraps()
 }
